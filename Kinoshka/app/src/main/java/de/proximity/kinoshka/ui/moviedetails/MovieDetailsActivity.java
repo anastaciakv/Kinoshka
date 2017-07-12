47d2152789feebf0ad9c358b40a628696d1fd39d
Anastasiia Korostelova <anastaciakv@gmail.com>
2017-07-12 13:53:05 +0200

    transparent statusbar

--- Kinoshka/app/src/main/java/de/proximity/kinoshka/ui/moviedetails/MovieDetailsActivity.java
+++ Kinoshka/app/src/main/java/de/proximity/kinoshka/ui/moviedetails/MovieDetailsActivity.java
@@ -7,6 +7,7 @@ import android.arch.lifecycle.ViewModelProviders;
 import android.databinding.DataBindingUtil;
 import android.os.Build;
 import android.os.Bundle;
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.Toast;
@@ -21,6 +22,7 @@ import de.proximity.kinoshka.databinding.ActivityMovieDetailsBinding;
 import de.proximity.kinoshka.di.Injectable;
 import de.proximity.kinoshka.entity.Movie;
 import de.proximity.kinoshka.ui.NavigationController;
+import de.proximity.kinoshka.utils.Helper;
 
 public class MovieDetailsActivity extends AppCompatActivity implements Injectable, LifecycleRegistryOwner, ImageReadyCallback {
     private final LifecycleRegistry lifecycleRegistry = new LifecycleRegistry(this);
@@ -65,6 +67,9 @@ public class MovieDetailsActivity extends AppCompatActivity implements Injectabl
 
     private void initView(Movie movie) {
         binding.toolbar.setTitle(movie.originalTitle);
+        CollapsingToolbarLayout.LayoutParams lp = (CollapsingToolbarLayout.LayoutParams) binding.toolbar.getLayoutParams();
+        lp.setMargins(0, Helper.getStatusBarHeight(this), 0, 0);
+        binding.toolbar.setLayoutParams(lp);
         setSupportActionBar(binding.toolbar);
         if (getSupportActionBar() != null) getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
@@ -75,11 +80,11 @@ public class MovieDetailsActivity extends AppCompatActivity implements Injectabl
         binding.setMovie(movie);
         binding.setViewModel(viewModel);
         //reviews list
-        binding.rvReviewList.setHasFixedSize(true);
-        binding.rvReviewList.setAdapter(reviewAdapter);
+        binding.extraInfo.rvReviewList.setHasFixedSize(true);
+        binding.extraInfo.rvReviewList.setAdapter(reviewAdapter);
         //trailers list
-        binding.rvTrailerList.setHasFixedSize(true);
-        binding.rvTrailerList.setAdapter(trailerAdapter);
+        binding.extraInfo.rvTrailerList.setHasFixedSize(true);
+        binding.extraInfo.rvTrailerList.setAdapter(trailerAdapter);
 
 
     }
--- Kinoshka/app/src/main/java/de/proximity/kinoshka/utils/Helper.java
+++ Kinoshka/app/src/main/java/de/proximity/kinoshka/utils/Helper.java
@@ -14,4 +14,13 @@ public class Helper {
         if (nColumns < 2) return 2;
         return nColumns;
     }
+
+    public static int getStatusBarHeight(Activity activity) {
+        int result = 0;
+        int resourceId = activity.getResources().getIdentifier("status_bar_height", "dimen", "android");
+        if (resourceId > 0) {
+            result = activity.getResources().getDimensionPixelSize(resourceId);
+        }
+        return result;
+    }
 }
